9d34466c854779f4c2247315f04ccd5a842467d3
code-syl <ejjvandelaar@gmail.com>
2023-06-12 10:54:39 +0200

    added test for output

--- hello-world/tests/cli.rs
+++ hello-world/tests/cli.rs
@@ -21,3 +21,9 @@ fn false_not_ok() {
     let mut command: Command = Command::cargo_bin(FALSE_EXECUTABLE).unwrap();
     command.assert().failure();
 }
+
+#[test]
+fn output() {
+    let mut command: Command = Command::cargo_bin(HELLO_WORLD_EXECUTABLE).unwrap();
+    command.assert().stdout("Hello, world!\n");
+}
