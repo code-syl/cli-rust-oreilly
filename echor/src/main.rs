e524aa4f09b8c77e631679e3fe29cf3f34822313
code-syl <ejjvandelaar@gmail.com>
2023-06-12 13:16:28 +0200

    Added basic print with args

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+const NEWLINE: char = '\n';
+
+#[derive(Parser, Default, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The text to display
+    #[arg(required = true)]
+    text: Vec<String>,
+
+    /// Omit the trailing newline
+    #[arg(short = 'n')]
+    omit_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Args::parse();
+    let mut text: String = args.text.join(" ");
+
+    if !args.omit_newline {
+        text.push(NEWLINE);
+    }
+
+    print!("{}", text);
 }
